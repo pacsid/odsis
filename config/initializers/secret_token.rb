662505a7957e573119fb8c468eb50eb2c9e84f54
Sidnei Pacheco <sidneip.junior@gmail.com>
2013-07-23 23:22:39 -0300

    init repo model user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odsis::Application.config.secret_token = 'b83cb91c002a489e73eec915896c8e66d562596abc96bf8c927870969df7577c586ece6f548fb3c09cd4911cac27056cb276568e649ce3b957e1d4f03b9c02f8'
